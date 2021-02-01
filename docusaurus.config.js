f5c1b94319514458f00314b80b055a726a395440
adamgayoso <adamgayoso@users.noreply.github.com>
2021-01-31 17:34:10 -0800

    move stylesheets

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,6 +11,15 @@ module.exports = {
   favicon: "img/favicon.ico",
   organizationName: "YosefLab", // Usually your GitHub org/user name.
   projectName: "scvi-tools-site", // Usually your repo name.
+  stylesheets: [
+    {
+      href: "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css",
+      type: "text/css",
+      integrity:
+        "sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X",
+      crossorigin: "anonymous",
+    },
+  ],
   themeConfig: {
     prism: {
       defaultLanguage: 'python',
@@ -102,14 +111,5 @@ module.exports = {
         },
       },
     ],
-  ],
-  stylesheets: [
-    {
-      href: "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css",
-      type: "text/css",
-      integrity:
-        "sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X",
-      crossorigin: "anonymous",
-    },
   ],
 };
