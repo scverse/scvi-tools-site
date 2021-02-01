9eb5cc88555cd9303a3ea6eab3b01ba3396bfe00
adamgayoso <adamgayoso@users.noreply.github.com>
2021-01-31 17:47:07 -0800

    katex

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,13 +13,6 @@ module.exports = {
   projectName: "scvi-tools-site", // Usually your repo name.
   stylesheets: [
     "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css",
-    {
-      href: "https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css",
-      type: "text/css",
-      integrity:
-        "sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X",
-      crossorigin: "anonymous",
-    },
   ],
   themeConfig: {
     prism: {
