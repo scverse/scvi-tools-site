43146a0effcceb8f9aa34ff5b59369546c4f5706
adamgayoso <adamgayoso@users.noreply.github.com>
2021-02-05 21:30:30 +0000

    Deploying to main from  @ 364509357f054cc121551a56061caa16dbd97f98 🚀

---
+++ d1daa5e9.81a10af2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[23],{96:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"autozi","name":"autozi","count":1,"permalink":"/blog/tags/autozi"}')}}]);
