1dceb6849fcbc32f6c17b55fee4b61440a2f4651
Adam Gayoso <adamgayoso@users.noreply.github.com>
2021-08-21 11:26:51 -0700

    update default image

--- src/theme/TeamCard/index.js
+++ src/theme/TeamCard/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import IconLink from "@theme/IconLink";
+import useBaseUrl from "@docusaurus/useBaseUrl"
 import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 import styles from "./styles.module.scss";
 
-const _DEFAULT_IMAGE_URL = "../../../static/img/logo.svg";
+const _DEFAULT_IMAGE_URL = useBaseUrl("`img/logo.svg`);
 
 function TeamCard(props) {
   const {
